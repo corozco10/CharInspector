4f371a49175f657a365139cacf495aa343ee607a
corozco10 <corozco10@mail.csuchico.edu>
2018-02-08 11:39:13 -0800

    Checking upper or lower case values

--- main.cpp
+++ main.cpp
@@ -1,24 +1,31 @@
-//Author:
+//Author:Christian Orozco
 #include<iostream>
 
 using namespace std;
 
 int main()
 {
-
+  char letter;
+  int number;
   cout<<"What character do you want to know about?\n";
-
-  //when user's entry is between A-Z...
-  cout<<" is an upper case letter!\n";
-
+  cin>>letter;
+  if(letter>='A' && letter<='Z')
+  {
+    //when user's entry is between A-Z...
+    cout<<" is an upper case letter!\n";
+  }
+  else if(letter>='a' && letter<='z')
+  {
   //when user's entry is between a-z...
   cout<<" is a lower case letter!\n";
-
+  }
+  else
+  {
   //in all other cases...
   cout<<"?! Pssh. What are you talking about?\n";
-  
+  }
   //no matter what they enter...
-  cout<<"The ASCII value is: ";
+  cout<<"The ASCII value is: "<<(int)letter<<endl;
 
   return 0;
 }
